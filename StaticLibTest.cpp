2293ece0370e728fd7fa3f09be8d0134793bd8d4
Senjable01 <jelbeans10@gmail.com>
2022-04-12 13:07:26 +0900

    first commit

---
+++ StaticLibTest.cpp
@@ -0,0 +1,12 @@
+﻿// StaticLibTest.cpp : スタティック ライブラリ用の関数を定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "StaticLibTest.h"
+
+// TODO: これは、ライブラリ関数の例です
+void fnStaticLibTest(float x, float y)
+{
+
+}
